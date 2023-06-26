3252d1ad33507b0095faeedb921e809d1d0f0d80
iktp <hexianke2020@gmail.com>
2023-06-26 18:31:11 +0200

    this version, seems I can get esccaping and deposited energy spectrum

---
+++ 124Te/build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/GeometryGeDet.cc" "CMakeFiles/sim.dir/GeometryGeDet.cc.o" "gcc" "CMakeFiles/sim.dir/GeometryGeDet.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/HPGeHit.cc" "CMakeFiles/sim.dir/HPGeHit.cc.o" "gcc" "CMakeFiles/sim.dir/HPGeHit.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/HPGeSD.cc" "CMakeFiles/sim.dir/HPGeSD.cc.o" "gcc" "CMakeFiles/sim.dir/HPGeSD.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/MyGunPrimaryGenerator.cc" "CMakeFiles/sim.dir/MyGunPrimaryGenerator.cc.o" "gcc" "CMakeFiles/sim.dir/MyGunPrimaryGenerator.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/MySensitiveDetector.cc" "CMakeFiles/sim.dir/MySensitiveDetector.cc.o" "gcc" "CMakeFiles/sim.dir/MySensitiveDetector.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/PrimaryGeneratorAction.cc" "CMakeFiles/sim.dir/PrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/sim.dir/PrimaryGeneratorAction.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/TrackingAction.cc" "CMakeFiles/sim.dir/TrackingAction.cc.o" "gcc" "CMakeFiles/sim.dir/TrackingAction.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/action.cc" "CMakeFiles/sim.dir/action.cc.o" "gcc" "CMakeFiles/sim.dir/action.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/construction.cc" "CMakeFiles/sim.dir/construction.cc.o" "gcc" "CMakeFiles/sim.dir/construction.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/event.cc" "CMakeFiles/sim.dir/event.cc.o" "gcc" "CMakeFiles/sim.dir/event.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/run.cc" "CMakeFiles/sim.dir/run.cc.o" "gcc" "CMakeFiles/sim.dir/run.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/sim.cc" "CMakeFiles/sim.dir/sim.cc.o" "gcc" "CMakeFiles/sim.dir/sim.cc.o.d"
+  "/home/iktp/Geant4-Work/NewStart/GammaProductionRateforDifferentMaterial/124Te/stepping.cc" "CMakeFiles/sim.dir/stepping.cc.o" "gcc" "CMakeFiles/sim.dir/stepping.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 124Te/build/CMakeFiles/sim.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim.dir/GeometryGeDet.cc.o"
+  "CMakeFiles/sim.dir/GeometryGeDet.cc.o.d"
+  "CMakeFiles/sim.dir/HPGeHit.cc.o"
+  "CMakeFiles/sim.dir/HPGeHit.cc.o.d"
+  "CMakeFiles/sim.dir/HPGeSD.cc.o"
+  "CMakeFiles/sim.dir/HPGeSD.cc.o.d"
+  "CMakeFiles/sim.dir/MyGunPrimaryGenerator.cc.o"
+  "CMakeFiles/sim.dir/MyGunPrimaryGenerator.cc.o.d"
+  "CMakeFiles/sim.dir/MySensitiveDetector.cc.o"
+  "CMakeFiles/sim.dir/MySensitiveDetector.cc.o.d"
+  "CMakeFiles/sim.dir/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/sim.dir/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/sim.dir/TrackingAction.cc.o"
+  "CMakeFiles/sim.dir/TrackingAction.cc.o.d"
+  "CMakeFiles/sim.dir/action.cc.o"
+  "CMakeFiles/sim.dir/action.cc.o.d"
+  "CMakeFiles/sim.dir/construction.cc.o"
+  "CMakeFiles/sim.dir/construction.cc.o.d"
+  "CMakeFiles/sim.dir/event.cc.o"
+  "CMakeFiles/sim.dir/event.cc.o.d"
+  "CMakeFiles/sim.dir/run.cc.o"
+  "CMakeFiles/sim.dir/run.cc.o.d"
+  "CMakeFiles/sim.dir/sim.cc.o"
+  "CMakeFiles/sim.dir/sim.cc.o.d"
+  "CMakeFiles/sim.dir/stepping.cc.o"
+  "CMakeFiles/sim.dir/stepping.cc.o.d"
+  "sim"
+  "sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
